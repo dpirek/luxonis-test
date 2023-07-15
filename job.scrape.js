b75680844aba9cd34aea5d9c1d0442868fcc9daa
Dave <dpirek@gmail.com>
2023-07-15 16:40:15 -0400

    vanilla version

---
+++ job.scrape.js
@@ -0,0 +1,30 @@
+
+import https from 'https';
+import fs from 'fs';
+
+const base = 'https://www.sreality.cz';
+const path = '/api/cs/v2/estates';
+const perPage = 500;
+const query = `?category_main_cb=2&category_type_cb=1&per_page=${perPage}&tms=1689448155345`;
+
+function request(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => resolve(data));
+    }).on('error', reject);
+  });
+}
+
+const scrape = async () => {
+  const response = await request(`${base}${path}${query}`);
+  const data = JSON.parse(response);
+
+  fs.writeFileSync('data.json', JSON.stringify(data._embedded.estates, null, 2));
+  console.log('Response Match:', data._embedded.estates.length === perPage);
+}
+
+scrape();
---
+++ server.js
@@ -0,0 +1,91 @@
+import http from 'http';
+import fs from 'fs';
+
+const PORT = 8080;
+const data = fs.readFileSync('data.json');
+const estates = JSON.parse(data);
+
+const formatPrice = (price) => {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+function Page({ estates, page }) {
+  const perPage = 9;
+  const spliced = estates.slice((page - 1) * perPage, page * perPage);
+
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Houses For Sale</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+      <main>
+
+        <section class="py-5 text-center container">
+          <div class="row py-lg-1">
+            <div class="col-lg-6 col-md-8 mx-auto">
+              <h1 class="fw-light">Houses</h1>
+              <p class="lead text-body-secondary">There are ${estates.length} houses listed.</p>
+            </div>
+          </div>
+        </section>
+    
+        <div class="album py-5 bg-body-tertiary">
+          <div class="container">
+      
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              ${spliced.map(estate => {
+                return `
+                <div class="col">
+                  <div class="card shadow-sm">
+                    <img width="100%" height="225" src="${estate?._links?.images[0]?.href}" />
+                    <div class="card-body">
+                      <p class="card-text">${estate.name} ${estate?.locality}</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <small class="text-body-secondary">${formatPrice(estate.price)} Kč</small>
+                      </div>
+                    </div>
+                  </div>
+                </div><!-- item -->`
+              }).join('')}
+            </div>
+            
+            <hr />
+
+            <nav>
+              <ul class="pagination">
+                ${page > 1 ? `<li class="page-item"><a class="page-link" href="/?page=${page - 1}">Previous</a></li>` : ''}
+                <li class="page-item"><a class="page-link" href="/?page=${page + 1}">Next</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </main>
+  
+      <footer class="text-body-secondary py-5">
+        <div class="container">
+          <p class="mb-1">Houses For Sale</p>
+        </div>
+      </footer>
+    </body>
+    </html>`;
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/favicon.ico') {
+    res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+    return res.end('');
+  }
+
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const page = Number(url.searchParams.get('page')) || 1;
+ 
+  res.end(Page({ estates, page: page }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
